211ac3942c764e07bc11e181212a7bade44d30e9
maximilianou <maximilianou@gmail.com>
2022-06-16 19:08:20 +0200

    feat(eth): code from hardhat tutorial, test ok

--- step2-hardhat/__tests__/Token.js
+++ step2-hardhat/__tests__/Token.js
@@ -1,6 +1,8 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+
+
 describe(`SHT Token contract`, () => {
   it(`Deployment should apply total ammount to the owner`, async () => {
     const [owner] = await ethers.getSigners();
@@ -9,4 +11,76 @@ describe(`SHT Token contract`, () => {
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   })
+});
+
+describe("Transactions", function() {
+
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    Token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens once its transaction has been
+    // mined.
+    hardhatToken = await Token.deploy();
+  });
+
+  it("Should transfer tokens between accounts", async function() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    // Transfer 50 tokens from owner to addr1
+    await hardhatToken.transfer(addr1.address, 50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+
+    // Transfer 50 tokens from addr1 to addr2
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+  });
+  it("Should fail if sender doesn’t Must have enough Tokens, sorry!", async function () {
+    const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+    // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
+    // `require` will evaluate false and revert the transaction.
+    await expect(
+      hardhatToken.connect(addr1).transfer(owner.address, 1)
+    ).to.be.revertedWith("Must have enough Tokens, sorry!");
+
+    // Owner balance shouldn't have changed.
+    expect(await hardhatToken.balanceOf(owner.address)).to.equal(
+      initialOwnerBalance
+    );
+  });
+  it("Should update balances after transfers", async function () {
+    const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+    // Transfer 100 tokens from owner to addr1.
+    await hardhatToken.transfer(addr1.address, 100);
+
+    // Transfer another 50 tokens from owner to addr2.
+    await hardhatToken.transfer(addr2.address, 50);
+
+    // Check balances.
+    const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
+
+    const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(100);
+
+    const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+    expect(addr2Balance).to.equal(50);
+  });
 });
